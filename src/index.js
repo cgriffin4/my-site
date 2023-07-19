a7e764d148ae4824d1098ec126528b1d46d6a186
Griffin <cgriffin@auburnalabama.org>
2023-07-19 12:32:00 -0500

    simplify

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { Layout } from './layout';
 import NotFound from './pages/404';
 import Home from './pages/home';
@@ -29,8 +28,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
